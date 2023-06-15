aa55e7164a5d38c51f86981cbf194fa93b44fae5
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-15 10:54:09 +0500

    WIP: just try to make the damn screen white

--- surfaces/main.cpp
+++ surfaces/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   SDL_Window *window = nullptr;
 
   // Grab the window surface
-  SDL_Surface *screen = nullptr;
+  SDL_Surface *screen;
 
   // Typically we want error checking like this.
   // Not that tutorials often omit these detais.
@@ -18,17 +18,19 @@ int main(int argc, char *argv[]) {
   }
 
   window =
-      SDL_CreateWindow("C++ SDL2 Window", 0, 0, 640, 480, SDL_WINDOW_SHOWN);
+      SDL_CreateWindow("C++ SDL2 Window", 20, 20, 640, 480, SDL_WINDOW_SHOWN);
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
   screen = SDL_GetWindowSurface(window);
-
-  // OpenGL graphics context
-  SDL_GLContext context;
-  context = SDL_GL_CreateContext(window);
 
   // Infinite loop for our application
   bool gameIsRunning = true;
   while (gameIsRunning) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    // SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
     SDL_Event event;
     // Start our event loop
     while (SDL_PollEvent(&event)) {
@@ -38,10 +40,17 @@ int main(int argc, char *argv[]) {
       }
 
       if (event.button.button == SDL_BUTTON_LEFT) {
-        SDL_LockSurface(screen);
         std::cout << "Left mouse was pressed\n";
-        SDL_memset(screen->pixels, 255, screen->h * screen->pitch);
+
+        // SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255,
+        // 255)); SDL_UpdateWindowSurface(window);
+
+        SDL_LockSurface(screen);
+        // SDL_memset(screen->pixels, 255, screen->h * screen->pitch);
+        SDL_FillRect(screen, NULL, 0);
+
         SDL_UnlockSurface(screen);
+
         SDL_UpdateWindowSurface(window);
       }
     }
